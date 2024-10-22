236e9591865b4e722b19decb37a83ac29d52f098
Ghazzete <164881262+Ghazzete@users.noreply.github.com>
2024-10-22 10:11:13 +0500

    Initialize Digital clock app

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import DigitalClock from "@/components/digital-clock";
+import React from "react";
+
+
+export default function Home() {
+  return (
+    <div>
+    <DigitalClock />
+    </div>
+  )
+}
